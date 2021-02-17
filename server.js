b1e5f0f265d5bbab9180bfc8abd287a64c8ac0d3
rayhanns <muhrayhanns04@gmail.com>
2021-02-17 09:55:09 +0700

    7

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const saltRounds = 10;
 const engines = require('consolidate');
 const paypal = require('paypal-rest-sdk');
 
-const port = process.env.PORT || '4090';
+const port = process.env.PORT || 4090;
 
 app.engine('ejs', engines.ejs);
 app.set('views', './src/views');
